8600c3670d6d80a788c3744bfad1b692c4a43592
TobeLeta <tobechi.chukwuleta@reventtechnologies.com>
2024-03-11 16:46:50 +0100

    cheque leaves controller

---
+++ ApplicationTest/Generators/FakeUtilityModel.cs
@@ -0,0 +1,13 @@
+﻿using ChequeMicroservice.Application.Common.Models;
+
+namespace Application.UnitTests.Generators
+{
+    public static class FakeUtilityModel
+    {
+
+        public static Result GetSuccessfulResult()
+        {
+            return Result.Success("Success");
+        }
+    }
+}
